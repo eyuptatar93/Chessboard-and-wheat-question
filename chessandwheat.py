032cb1f709928808df1af2712b6dddf8a696da71
Muhammet Eyyüp Tatar <64421764+eyuptatar93@users.noreply.github.com>
2022-03-30 18:51:37 +0300

    Create chessandwheat.py

---
+++ chessandwheat.py
@@ -0,0 +1,24 @@
+list=[]
+
+for i in range(1,65):
+    if i== 1:
+        list.append(i)
+        
+    elif i ==2:
+        list.append(i)
+        
+    elif i ==3:
+        list.append(4)
+        
+    else:
+        list.append(2*list[-1]) 
+        
+total=0
+
+print(list)
+
+for i in list:
+    total+=i
+
+print(total)
+        
