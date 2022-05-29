7879a48f9ccd519b9ec54518368bf8403e196c75
Muhammet Eyyüp Tatar <64421764+eyuptatar93@users.noreply.github.com>
2022-05-30 02:15:27 +0300

    Update chessandwheat.py

--- chessandwheat.py
+++ chessandwheat.py
@@ -2,14 +2,7 @@ list=[]
 
 for i in range(1,65):
     if i== 1:
-        list.append(i)
-        
-    elif i ==2:
-        list.append(i)
-        
-    elif i ==3:
-        list.append(4)
-        
+        list.append(i)      
     else:
         list.append(2*list[-1]) 
         
